87111154dcf76d4fd86e058bc55a0620054e185b
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-06-24 11:53:13 +0530

    finishing the alignment

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 import Slidercomponent from "../components/slider"
 import MentorsCOmponent from '../components/image'
 import IndustriesandSpecializationcomponent from '../components/specilizationindustries'
-import EnquiryForm from '../components/enquryform'
+import ContactComponent from '../components/contactus'
 import TitleComponent from '../components/title'
 
 const IndexPage = () => (
@@ -80,21 +80,7 @@ const IndexPage = () => (
     </div>
     <br/>
     <hr style={{marginTop:"0px"}}/>
-    <div css={main1}>
-       <div css={one}>
-          <div style={{textAlign:"center"}}>
-          <TitleComponent title="Location"/>  
-          </div>
-       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317718.69319292053!2d-0.3817765050863085!3d51.528307984912544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sin!4v1592900207891!5m2!1sen!2sin" width="100%" height="530" frameborder="0" tabindex="0"></iframe>
-       </div>
-       <div  css={two}>
-         <div style={{textAlign:"center"}}>
-         <TitleComponent title="Intrested in Getting in touch?"/>
-         </div>
-       <EnquiryForm/>
-       </div>
-      
-      </div>
+    <ContactComponent/>
       
   </Layout>
 )
