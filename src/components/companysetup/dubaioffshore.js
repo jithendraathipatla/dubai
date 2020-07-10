311dbef852db0d8f15f5c0bff13504e74cf2463d
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-07-10 07:55:01 +0530

    finishing touches

--- src/components/companysetup/dubaioffshore.js
+++ src/components/companysetup/dubaioffshore.js
@@ -1,60 +1,139 @@
 import React from "react"
-import BusinessSetupHOC from "../HOC/business-setup-offshore";
+import BusinessSetupHOC from "../HOC/business-setup-offshore"
+import { FaDotCircle } from "react-icons/fa"
+import { css } from "@emotion/core"
 
 const BusinessSetupInDiera = () => {
   return (
     <>
       <BusinessSetupHOC
-        main="BUSINESS SETUP IN DUBAI INTERNATIONAL FINANCIAL CENTRE FREE ZONE"
-        submain="The global financial center of the middle-east, DIFC is the middle eastern hub for financial institutes"
-        hetchfour="BUSINESS SETUP IN DUBAI INTERNATIONAL FINANCIAL CENTRE FREE ZONE DUBAI"
+        main="OFFSHORE COMPANY FORMATION IN DUBAI"
+        submain="Hassle-free, tax-free, and secure offshore company formation in Dubai"
+        hetchfour="OFFSHORE COMPANY FORMATION IN DUBAI"
       >
         <div>
           <br />
           <span>
             <h1 style={{ fontSize: "22px", color: "black" }}>
-              The global financial center of the middle-east, DIFC is the middle
-              eastern hub for financial institutes
+              Hassle-free, tax-free, and secure offshore company formation in
+              Dubai
             </h1>
             <br />
-            The global financial center of the middle-east, Dubai International
-            Financial Centre is a monetarist hub creating platform for companies
-            and financial institutes to establish themselves in these developing
-            region. Company formation in Dubai International Financial Centre
-            Free Zone provides mutual frameworks, tax-friendly benefits,
-            opportune laws and regimes. The Dubai International Financial Centre
-            Freezone is also called as DIFC free zone. The jurisdiction is among
-            the key players encouraging foreign investors to tap the
-            international markets, through investing in the region. The region
-            also provides authenticity and regularities along with proficiency
-            in conducting a Business setup in Dubai International Financial
-            Centre Freezone. Established just a decade old, the DIFC free zone,
-            has become amongst the leading financial centers, contributing over
-            12 percent on Dubai’s GDP which demonstrates the rapid expansion.
-            Free zone company registration in Dubai International Financial
-            Centre permits major financial services authorities, financial
-            SME’s, financial institutes, banks and pecuniary commercial
-            entities, such as – Al Tamimi & Company, Dubai International
-            Financial Centre (DIFC), Investment Corporation of Dubai (ICD),
-            Industrial Commercial Bank of China, Freshfields Bruckhaus Deringer
-            LLP, Baker & McKenzie. Habib Al Mulla DIFC, Clifford Chance Dubai,
-            Asteco Property Management, Dubai Head Office, Brunswick Group,
-            Peninsula Business Solutions Ltd among others.
+            A tax haven like no other is what UAE offshore businesses enjoy. A
+            lot of countries provide tax benefits and are known to protect the
+            assets and financial information of high net-worth individuals.
+            However, Dubai offshore has emerged as the best and most secure tax
+            haven for company incorporation.
             <br />
             <br />
-            The DIFC is an independent regulator under the guidance of Dubai
-            Financial Services Authority and offers a world-class financial
-            services centre. Moreover, the diversification components of DIFC
-            along with the capital proposition providing significant
-            opportunities, also plays a vital role in the international finance
-            system. DIFC expands the ecosystem ensuring that the business
-            members in the jurisdiction take advantage of the new prospects in
-            the emerging markets as well as emphasizes alternative financial
-            mechanisms. DIFC continues to entice financial communities as a base
-            from which to carry out business – if you are appealed as well
-            contact Shuraa business setup, a prominent business setup
-            consultancy in Dubai offering you innovative solutions with regards
-            to company formation in UAE.
+            Dubai offshores are one of the few tax havens that has not signed
+            any treaties to disclose financial information of offshore clients
+            to home countries of these individuals. Dubai offshore regulator
+            does not keep a public register of shareholders and directors and
+            can never be forced to reveal your company information to other
+            countries or tax authorities. When you register an offshore company
+            in Dubai, you are a legal bona fide business, which gives you an
+            easy access to conducting business in the Middle East, anywhere
+            outside UAE.
+            <br />
+            <br />
+            Sevago Business Setup helps you with offshore company registration
+            in Dubai. We get you the incorporation certificates and other
+            government approvals, so you can set up an offshore company in Dubai
+            in the safest and quickest way. Our Dubai offshore company formation
+            packages are the most hassle-free and competitively priced ones.
+            Sevago Business Setup’s offshore business setup Dubai packages offer
+            quick, safe and hassle-free solutions. We help you register your
+            company, get company incorporation certificate, offshore bank
+            accounts and everything else that you would require to setup your
+            offshore company.
+            <br />
+            <br />
+            <h1 style={{ fontSize: "22px", color: "black" }}>
+              START A DUBAI OFFSHORE COMPANY WITH SEVAGO BUSINESS SETUP AND GET
+            </h1>
+            <br />
+            <h2 style={{ fontSize: "18px", color: "black" }}>
+              100% Foreign Ownership
+            </h2>
+            <br />
+            With Sevago Business Setup you can start a company in Dubai
+            offshores. You will not only get 100 % ownership but can also
+            channel your revenue with offshore company bank accounts. Offshore
+            company formation in Dubai also provides you 100% ownership of a
+            property in the UAE as well as overseas.
+            <br />
+            <br />
+            <h2 style={{ fontSize: "18px", color: "black" }}>
+              Benefit of Confidentiality
+            </h2>
+            <br />
+            Setup a Dubai offshore company with Sevago Business Setup and get
+            the benefit of confidentiality. The country provides strict
+            corporate laws and banking confidentiality, that allows a
+            businessman to keep his company hidden. While forming an offshore
+            company in Dubai the judicial bodies will only disclose the identity
+            in case of illegal activities.
+            <br />
+            <br />
+            <h2 style={{ fontSize: "18px", color: "black" }}>
+              Incentives on Taxation
+            </h2>
+            <br />
+            Dubai has designed favourable incentives providing tax advantageous
+            options to entrepreneurs on Dubai offshore company formation. The
+            agenda to do so is to promote healthy entrepreneurial environment to
+            attract foreign investments. Take advantage of this tax haven, find
+            out more from Sevago Business Setup.
+            <br />
+            <br />
+            <h1 style={{ fontSize: "22px", color: "black" }}>
+              WHAT ARE THE BENEFITS OF COMPANY FORMATION IN DUBAI OFFSHORE?
+            </h1>
+            <br />
+            <ul css={list}>
+              <li>
+                <FaDotCircle /> Easy Company Incorporation
+              </li>
+              <li>
+                <FaDotCircle /> 100% Tax Free UAE Company Setup
+              </li>
+              <li>
+                <FaDotCircle /> 100% Foreign Ownership of Company
+              </li>
+              <li>
+                <FaDotCircle /> 100% Return of Capital & Profits
+              </li>
+              <li>
+                <FaDotCircle /> Low Cost Company Setup
+              </li>
+              <li>
+                <FaDotCircle /> No Export or Import Duty
+              </li>
+              <li>
+                <FaDotCircle /> No Mandatory Financial Reporting
+              </li>
+              <li>
+                <FaDotCircle /> Company Setup with Full Privacy
+              </li>
+            </ul>
+            <br />
+            <br />
+            <h1 style={{ fontSize: "22px", color: "black" }}>
+              WHY START AN OFFSHORE COMPANY IN DUBAI WITH SEVAGO?
+            </h1>
+            <br />
+            With Sevago Business Setup – UAE offshore company incorporation in
+            Dubai is easy, affordable and effective. Simply start a Dubai
+            offshore company with us to get complete solutions on offshore
+            company registration, Run your internationals business in the most
+            competent mode and enjoy full exemption from taxation in the UAE
+            <br />
+            <br />
+            Did you know with offshore company registration in Dubai you get
+            complete foreign ownership and are permitted to open bank accounts
+            inside the country as well as abroad. To find out more about
+            offshore business setup in Dubai, speak to our consultant for Free.
           </span>
           <br />
           <br />
@@ -65,3 +144,10 @@ const BusinessSetupInDiera = () => {
 }
 
 export default BusinessSetupInDiera
+
+const list = css`
+  svg {
+    font-size: 10px;
+    padding-top: -5px;
+  }
+`
